8374316ebdb31a63d3b7409085c21e6e477bf37b
Muneeb Hassan <imuneebhassan6@gmail.com>
2026-07-08 11:17:37 +0500

    fixed LoginForm.css case sensitivity

--- Frontend/src/Feature/Auth/LoginForm.jsx
+++ Frontend/src/Feature/Auth/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaUserShield, FaLock, FaUser, FaArrowLeft, FaEye, FaEyeSlash } from "react-icons/fa";
 import Swal from "sweetalert2";
-import "./LoginForm.css";
+import "./loginForm.css";
 
 const LoginForm = () => {
   const navigate = useNavigate();
