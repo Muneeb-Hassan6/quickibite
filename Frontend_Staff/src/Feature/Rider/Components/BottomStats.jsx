1c7e3a9e8446e23759997d40bfe20f92f2f112d6
Faiz Ul Hassan <fm4133196@gmail.com>
2026-08-15 18:37:10 +0500

    Saved local changes in BottomStats before merging

--- Frontend_Staff/src/Feature/Rider/Components/BottomStats.jsx
+++ Frontend_Staff/src/Feature/Rider/Components/BottomStats.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BottomStats = ({ stats }) => {
     return (
-        <div className="absolute bottom-0 left-0 w-full bg-[var(--admin-panel)]  flex justify-around p-[15px_0_20px_0] z-[100]">
+        <div className="absolute bottom-0 left-0 w-full bg-[var(--admin-panel)] border-t border-[var(--admin-border)] flex justify-around p-[15px_0_20px_0] z-[100]">
             <div className="text-center">
                 <div className="text-[var(--admin-text)] text-[22px] font-oswald">{stats.deliveries}</div>
                 <div className="text-[var(--admin-muted)] text-[11px] font-semibold mt-[5px] uppercase">Deliveries</div>
